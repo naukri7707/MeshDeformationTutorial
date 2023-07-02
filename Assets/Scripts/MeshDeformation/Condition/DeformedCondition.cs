faf5932fbadc999dc39607b419fe738964774ddb
Naukri <naukri7707@gmail.com>
2023-07-03 07:04:56 +0800

    新增 ShaderPassLayer 釋例

---
+++ Assets/Scripts/MeshDeformation/Condition/DeformedCondition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Naukri.MeshHelper
+{
+    public class DeformedCondition : ShaderPassLayerCondition
+    {
+        public float triggerDistance;
+
+        protected override bool OnEvaluation(Args args)
+        {
+            var disA = Vector3.Distance(args.triangle.a, args.originalTriangle.a);
+            var disB = Vector3.Distance(args.triangle.b, args.originalTriangle.b);
+            var disC = Vector3.Distance(args.triangle.c, args.originalTriangle.c);
+            var distance = (disA + disB + disC) / 3;
+            if (args.triangle != args.originalTriangle)
+            {
+                Debug.Log($"{distance}:{distance > triggerDistance}");
+            }
+            return distance > triggerDistance;
+        }
+    }
+}
