5d8fba6784117e8c9d06ff6eee57718eced5ff2a
Naukri <naukri7707@gmail.com>
2023-07-03 02:50:02 +0800

    新增 VertexModifier 釋例

--- Assets/Scripts/MeshDeformation/DeformStyle.cs
+++ Assets/Scripts/MeshDeformation/DeformStyle.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 
-[Flags]
-public enum DeformStyle
+namespace Naukri.MeshHelper
 {
-    None = 0,
-    Right = 1 << 0,
-    Left = 1 << 1,
-    Up = 1 << 2,
-    Down = 1 << 3,
-    Forward = 1 << 4,
-    Back = 1 << 5,
-    XAxis = Right | Left,
-    YAxis = Up | Down,
-    ZAxis = Forward | Back,
-    XYFace = XAxis | YAxis,
-    XZFace = XAxis | ZAxis,
-    YZFace = YAxis | ZAxis,
-    All = XAxis | YAxis | ZAxis,
+    [Flags]
+    public enum DeformStyle
+    {
+        None = 0,
+        Right = 1 << 0,
+        Left = 1 << 1,
+        Up = 1 << 2,
+        Down = 1 << 3,
+        Forward = 1 << 4,
+        Back = 1 << 5,
+        XAxis = Right | Left,
+        YAxis = Up | Down,
+        ZAxis = Forward | Back,
+        XYFace = XAxis | YAxis,
+        XZFace = XAxis | ZAxis,
+        YZFace = YAxis | ZAxis,
+        All = XAxis | YAxis | ZAxis,
+    }
 }
--- Assets/Scripts/MeshDeformation/DeformableObject.cs
+++ Assets/Scripts/MeshDeformation/DeformableObject.cs
@@ -11,11 +11,42 @@ namespace Naukri.MeshHelper
 
         public MeshRenderer MeshRenderer { get; private set; }
 
+        public VertexModifier[] vertexModifiers;
+
         // Start is called before the first frame update
-        void Awake()
+        private void Awake()
         {
             MeshFilter = GetComponent<MeshFilter>();
             MeshRenderer = GetComponent<MeshRenderer>();
+            // 使用克隆體進行計算，避免多個 DeformableObject 同時使用一個 VertexModifier 的情況
+            for (int i = 0; i < vertexModifiers.Length; i++)
+            {
+                vertexModifiers[i] = Instantiate(vertexModifiers[i]);
+            }
+        }
+
+        private void Start()
+        {
+            // 初始化 VertexModifier
+            foreach (var vertexModifier in vertexModifiers)
+            {
+                vertexModifier.InitialImpl(this);
+            }
+        }
+
+        public Vector3 ModifyVertex(VertexModifyArgs args)
+        {
+            // 執行 VertexModifier
+            foreach (var modifier in vertexModifiers)
+            {
+                modifier.OnVertexModifyImpl(args);
+                // 如果被標註為已完成則跳出迴圈
+                if(args.IsCompleted)
+                {
+                    break;
+                }
+            }
+            return args.vector;
         }
     }
 }
