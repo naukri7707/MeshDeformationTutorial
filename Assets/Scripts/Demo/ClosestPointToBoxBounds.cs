4fcc8c15325c2b89d9a8cc41d326d93d76d9e506
Naukri <naukri7707@gmail.com>
2023-07-02 11:38:27 +0800

    新增 FaceInfo 紀錄面資料

--- Assets/Scripts/Demo/ClosestPointToBoxBounds.cs
+++ Assets/Scripts/Demo/ClosestPointToBoxBounds.cs
@@ -17,20 +17,20 @@ public class ClosestPointToBoxBounds : MonoBehaviour
 
         if (isInsideCollider)
         {
-            var faces = GetFaceInfo();
+            var FaceInfos = GetFaceInfoInfo();
 
             var closestDistance = float.MaxValue;
             var closestPoint = Vector3.zero;
             var closestIndex = -1;
             // 計算投影平面的法向量
-            for (int i = 0; i < faces.Length; i++)
+            for (int i = 0; i < FaceInfos.Length; i++)
             {
-                var originPoint = faces[i].origin;
+                var originPoint = FaceInfos[i].origin;
 
                 // 目標點之於平面原點的向量
                 var direction = targetPoint - originPoint;
 
-                var planeNormal = faces[i].normal;
+                var planeNormal = FaceInfos[i].normal;
                 planeNormal.Normalize();
 
                 // 投影目標點至平面並取得向量 (相對於平面原點的位置)
@@ -71,21 +71,21 @@ public class ClosestPointToBoxBounds : MonoBehaviour
     }
 
 
-    public Face[] GetFaceInfo()
+    public FaceInfo[] GetFaceInfoInfo()
     {
 
         var center = boxCollider.bounds.center;
         var extents = boxCollider.bounds.extents;
 
-        var faces = new Face[6];
+        var FaceInfos = new FaceInfo[6];
 
-        faces[0] = new Face(center + new Vector3(extents.x, 0, 0), transform.right);
-        faces[1] = new Face(center + new Vector3(-extents.x, 0, 0), -transform.right);
-        faces[2] = new Face(center + new Vector3(0, extents.y, 0), transform.up);
-        faces[3] = new Face(center + new Vector3(0, -extents.y, 0), -transform.up);
-        faces[4] = new Face(center + new Vector3(0, 0, extents.z), transform.forward);
-        faces[5] = new Face(center + new Vector3(0, 0, -extents.z), -transform.forward);
+        FaceInfos[0] = new FaceInfo(center + new Vector3(extents.x, 0, 0), transform.right);
+        FaceInfos[1] = new FaceInfo(center + new Vector3(-extents.x, 0, 0), -transform.right);
+        FaceInfos[2] = new FaceInfo(center + new Vector3(0, extents.y, 0), transform.up);
+        FaceInfos[3] = new FaceInfo(center + new Vector3(0, -extents.y, 0), -transform.up);
+        FaceInfos[4] = new FaceInfo(center + new Vector3(0, 0, extents.z), transform.forward);
+        FaceInfos[5] = new FaceInfo(center + new Vector3(0, 0, -extents.z), -transform.forward);
 
-        return faces;
+        return FaceInfos;
     }
 }
---
+++ Assets/Scripts/MeshDeformation/FaceInfo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public readonly struct FaceInfo
+{
+    public readonly Vector3 origin;
+
+    public readonly Vector3 normal;
+
+    public FaceInfo(Vector3 origin, Vector3 normal)
+    {
+        this.origin = origin;
+        this.normal = normal;
+    }
+}
